c6ca86fafb8a4db19589a62ca0fef32c90e68b70
OleksiiJS <alexey.kriachok@gmail.com>
2023-10-09 18:56:29 +0300

    K@K K@K@Tb L@L

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-const app : Number = 1;
+import { Telegraf } from 'telegraf'
+import { message } from 'telegraf/filters';
+
+const bot = new Telegraf('6592560907:AAG2IiuT_bqs5ooOBldZw_Zto2mWSTF_z1Q');
+
+
+bot.on(message("text"), async ctx => {
+
+    await ctx.reply("Hello")
+});
+
+
+bot.launch()
